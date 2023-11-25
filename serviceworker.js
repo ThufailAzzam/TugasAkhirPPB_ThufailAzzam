e7baa4f4e9f4b0c04b8247241b6093323cf62904
ThufailAzzam <102534663+ThufailAzzam@users.noreply.github.com>
2023-11-25 22:17:18 +0700

    first commit

---
+++ serviceworker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'umacache';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/aboot.html',
+  '/umadetail.html',
+  '/music.html',
+  '/musicdetail.html',
+  '/styles.css',
+  '/manifest.json',
+  '/manifest-icon-192.maskable.png',
+  '/manifest-icon-512.maskable.png',
+  '/x2.png',
+
+];
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request because it's a stream and can only be consumed once.
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response because it's a stream and can only be consumed once.
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
